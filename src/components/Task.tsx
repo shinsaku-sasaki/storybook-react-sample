2e9d8523422cfa484c41c052ad02cea04e158a6d
shinsaku-sasaki <shinsakusasakiengineer39@gmail.com>
2024-08-30 20:05:26 +0900

    change to white

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -38,7 +38,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
                     name="title"
                     id={`title-${id}`}
                     placeholder="Input title"
-                    style={{ backgroundColor: 'red' }}
+                    style={{ backgroundColor: 'white' }}
                 />
             </label>
             {state !== "TASK_ARCHIVED" && (
